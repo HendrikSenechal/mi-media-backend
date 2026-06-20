44d66a62a74bc39ced93de60374c3482e13b4738
HendrikSenechal <hendrik.senechal@gmail.com>
2026-06-20 16:52:05 +0200

    Role service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthController } from './health/health.controller';
 import { RoleController } from './role/role.controller';
+import { RoleService } from './role/role.service';
 
 @Module({
   imports: [],
   controllers: [AppController, HealthController, RoleController],
-  providers: [AppService],
+  providers: [AppService, RoleService],
 })
 export class AppModule {}
